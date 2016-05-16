def29f51467c973cc67672b16a40c9c9ec706f93
aikenyang <aikenyang@hotmail.com>
2016-05-16 17:38:15 +0800

    no version

--- recipes/mongodb-org-amazon-32.rb
+++ recipes/mongodb-org-amazon-32.rb
@@ -6,26 +6,4 @@ end
 
 yum_package 'mongodb-org' do
 	action :install
-	version '3.2.6'
 end
-
-yum_package 'mongodb-org-server' do
-	action :install
-	version '3.2.6'
-end
-
-yum_package 'mongodb-org-shell' do
-	action :install
-	version '3.2.6'
-end
-
-yum_package 'mongodb-org-mongos' do
-	action :install
-	version '3.2.6'
-end
-
-yum_package 'mongodb-org-tools' do
-	action :install
-	version '3.2.6'
-end
-     
