93aa1d26f3cf23d33e12e7cbb6e76a53ea02060b
aikenyang <aikenyang@hotmail.com>
2016-05-16 17:27:20 +0800

    change to yum_package

--- recipes/mongodb-org-amazon-32.rb
+++ recipes/mongodb-org-amazon-32.rb
@@ -4,23 +4,23 @@ cookbook_file "/etc/yum.repos.d/mongodb-org-3.2.repo" do
   action :create_if_missing
 end
 
-package 'mongodb-org-3.2.6' do
+yum_package 'mongodb-org-3.2.6' do
 	action :install
 end
 
-package 'mongodb-org-server-3.2.6' do
+yum_package 'mongodb-org-server-3.2.6' do
 	action :install
 end
 
-package 'mongodb-org-shell-3.2.6' do
+yum_package 'mongodb-org-shell-3.2.6' do
 	action :install
 end
 
-package 'mongodb-org-mongos-3.2.6' do
+yum_package 'mongodb-org-mongos-3.2.6' do
 	action :install
 end
 
-package 'mongodb-org-tools-3.2.6' do
+yum_package 'mongodb-org-tools-3.2.6' do
 	action :install
 end
      
