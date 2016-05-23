109cdcc32cfd7beaabfa32f538803c146f62b812
Kenneth Yang (DCS-TW) <kenneth_yang@tw.trendnet.org>
2016-05-23 23:42:06 +0800

    addfiles

--- recipes/mongodb-org-amazon-32.rb
+++ recipes/mongodb-org-amazon-32.rb
@@ -14,9 +14,27 @@ end
       cat /tmp/parms_to_append.conf >> /etc/security/limits.conf
       rm /tmp/parms_to_append.conf
    EOF
-   not_if "grep -q nproc /etc/security/limits.conf"
+   not_if "grep -q 32000 /etc/security/limits.conf"
  end
 
+ cookbook_file "/etc/security/limits.d/90-nproc.conf" do
+  source "90-nproc.conf"
+  mode 0644
+  action :create_if_missing
+end
+ 
+ cookbook_file "/etc/udev/rules.d/85-ebs.rules" do
+  source "85-ebs.rules"
+  mode 0644
+  action :create_if_missing
+end 
+ 
+cookbook_file "/etc/init.d/disable-transparent-hugepages" do
+  source "disable-transparent-hugepages"
+  mode 0755
+  action :create_if_missing
+end 
+ 
 yum_package 'mongodb-org' do
 	action :install
 	version '3.2.6-1.amzn1'
