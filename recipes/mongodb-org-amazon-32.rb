904c0886505830bfe0e2dbbde8e350b6e4d52a3c
Kenneth Yang (DCS-TW) <kenneth_yang@tw.trendnet.org>
2016-05-23 20:44:08 +0800

    limit

--- recipes/mongodb-org-amazon-32.rb
+++ recipes/mongodb-org-amazon-32.rb
@@ -4,6 +4,19 @@ cookbook_file "/etc/yum.repos.d/mongodb-org-3.2.repo" do
   action :create_if_missing
 end
 
+ cookbook_file "/tmp/parms_to_append.conf" do
+   source "limits.conf"
+ end
+
+ bash "append_to_config" do
+   user "root"
+   code <<-EOF
+      cat /tmp/parms_to_append.conf >> /etc/security/limits.conf
+      rm /tmp/parms_to_append.conf
+   EOF
+   not_if "grep -q nproc /etc/security/limits.conf"
+ end
+
 yum_package 'mongodb-org' do
 	action :install
 	version '3.2.6-1.amzn1'
