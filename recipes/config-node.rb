4a9e8fb53581c087d9adb9da3eba438a2fefc9d1
Kenneth Yang (DCS-TW) <kenneth_yang@tw.trendnet.org>
2016-06-09 23:08:53 +0800

    .

--- recipes/config-node.rb
+++ recipes/config-node.rb
@@ -8,25 +8,42 @@ bash "install_jp" do
   EOF
 end
 
-#receive message from SQS
-bash "get_ip_from_sqs" do
+#generate the replica set config file 'init.js'
+bash "gen_init.js" do
   user "root"
   code <<-EOF
-    ip1=$(aws sqs receive-message --queue-url https://sqs.us-east-1.amazonaws.com/595704032741/mongod-az1 --max-number-of-messages 1 --region us-east-1)
+	ip1=$(aws sqs receive-message --queue-url https://sqs.us-east-1.amazonaws.com/595704032741/mongod-az1 --max-number-of-messages 1 --region us-east-1)
 	host1=$(echo $ip1 | /usr/bin/jq '.Messages[0].Body')
 	host1=$(echo $host1|sed 's/\"//g')
-	host1="'$host1:27017'"
-	
+	#host1="11.2.3.4"
+
 	ip2=$(aws sqs receive-message --queue-url https://sqs.us-east-1.amazonaws.com/595704032741/mongod-az1 --max-number-of-messages 1 --region us-east-1)
 	host2=$(echo $ip2 | /usr/bin/jq '.Messages[0].Body')
 	host2=$(echo $host2|sed 's/\"//g')
-	host2="'$host2:27017'"
-	
-	ip0=$(curl http://169.254.169.254/latest/meta-data/local-ipv4/)
-	host0="$ip0:27017"
-	str=$"cfg = {_id:'RS1', members:[{_id:0, host:'"$host0"'}, {_id:1, host:"$host1"}, {_id:2, host:"$host2"}]}"
-	echo $str > /data/init.js
-	echo 'rs.initiate(cfg)' >> /data/init.js
+	#host2="11.2.3.4"
+		
+	for (( i=1; i<=5; i++ ))
+	do
+	 echo "try $i time(s)..."
+	 if [ ! -z $host1 ] && [ ! -z $host2 ]; then
+	  host1="'$host1:27017'"
+	  host2="'$host2:27017'"
+	  #echo "host1 is $host1"
+	  #echo "host2 is $host2"
+	  
+	  ip0=$(curl http://169.254.169.254/latest/meta-data/local-ipv4/)
+	  host0="$ip0:27017"
+	  
+	  str=$"cfg = {_id:'RS1', members:[{_id:0, host:'"$host0"'}, {_id:1, host:"$host1"}, {_id:2, host:"$host2"}]}"
+	  echo $str > /data/init.js
+	  
+	  break
+	 else
+	  echo "one of them is not ready"
+	  sleep 60
+	 fi
+	done
+
   EOF
 end
 
