f151bee8412fa663beef4709a433c5d7b26095d2
Kenneth Yang (DCS-TW) <kenneth_yang@tw.trendnet.org>
2016-06-16 16:56:16 +0800

    .

--- recipes/testing.rb
+++ recipes/testing.rb
@@ -1,9 +1,10 @@
 region = node["opsworks"]["instance"]["region"]
 hostname = node["opsworks"]["instance"]["hostname"]
-environment_type = node["deploy"]["arq"]["environment_variables"]["environment_type"]
+#environment_type = node["deploy"]["arq"]["environment_variables"]["environment_type"]
 
 file '/tmp/file' do
-  content 'region=#{region}; hostname=#{hostname}; environment_type=#{environment_type}'
+  #content 'region=#{region}; hostname=#{hostname}; environment_type=#{environment_type}'
+  content 'region=#{region}; hostname=#{hostname}}'
   mode '0755'
   owner 'root'
   grout 'root'
