feca09b9783aa7ee3351a921d958375989b7e0f0
Kenneth Yang (DCS-TW) <kenneth_yang@tw.trendnet.org>
2016-06-17 17:34:30 +0800

    .

--- recipes/testing.rb
+++ recipes/testing.rb
@@ -2,10 +2,17 @@ region = node["opsworks"]["instance"]["region"]
 hostname = node["opsworks"]["instance"]["hostname"]
 #environment_type = node["deploy"]["arq"]["environment_variables"]["environment_type"]
 
-file '/tmp/file' do
+#file '/tmp/file' do
   #content 'region=#{region}; hostname=#{hostname}; environment_type=#{environment_type}'
   content 'region=#{region}; hostname=#{hostname}}'
-  mode '0755'
-  owner 'root'
-  grout 'root'
-end
+#  mode '0755'
+#  owner 'root'
+#  grout 'root'
+#end
+
+ bash "echo_hostname" do
+   user "root"
+   code <<-EOF
+      echo #{region}
+   EOF
+ end
