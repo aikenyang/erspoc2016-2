e329140e6c712d1bf7456338ebbc9d51aef65e6e
Kenneth Yang (DCS-TW) <kenneth_yang@tw.trendnet.org>
2016-06-17 18:13:29 +0800

    .

--- recipes/testing.rb
+++ recipes/testing.rb
@@ -1,7 +1,14 @@
-require 'aws-sdk'
+#require 'aws-sdk'
 
-region = node["opsworks"]["instance"]["region"]
-hostname = node["opsworks"]["instance"]["hostname"]
+#region = node["opsworks"]["instance"]["region"]
+#hostname = node["opsworks"]["instance"]["hostname"]
 
-Chef::Log.info("deploy #{region}")
-Chef::Log.info("deploy #{hostname}")
+#Chef::Log.info("deploy #{region}")
+#Chef::Log.info("deploy #{hostname}")
+
+
+bash "echo something" do
+   code <<-EOF
+     echo 'I am a chef!'
+   EOF
+end
